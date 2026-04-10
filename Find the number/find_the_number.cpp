da8c23fc058c1e4f7cb19f7f5cca6774726974c1
stCristi <cristianstefanut60@gmail.com>
2026-04-10 12:45:14 +0200

    find the number(game)  update 1.0!

--- Find the number/find_the_number.cpp
+++ Find the number/find_the_number.cpp
@@ -5,35 +5,39 @@ using namespace std;
 
 int main() {
     int number = 0, attempt = 0, attemptnumber = 10;
-
+    cout<<"Let the game begin!"<<endl;
     srand(time(0));
     int randomnumber = rand() % 100 + 1;
 
+    int low = 1;
+    int high = 100;
+
     while (attempt < attemptnumber) {
+        cout << "Guess the number (" << low << "<->" << high << "): ";
         cin >> number;
 
         if (number < 1 || number > 100) {
             cout << "The number needs to be between 1 and 100" << endl;
+            continue;
         }
 
+        attempt++;
+
+        if (number < randomnumber) {
+            cout << "No, the number is HIGHER" << endl;
+            low = number + 1;   
+        }
+        else if (number > randomnumber) {
+            cout << "No, the number is LOWER" << endl;
+            high = number - 1;  
+        }
         else {
-            attempt++;
-
-            if (number < randomnumber) {
-                cout << "No, the number is higher +" << endl;
-            }
-            
-            else if (number > randomnumber) {
-                cout << "No, the number is lower -" << endl;
-            }
-            
-            else {
-                cout << "BOOYAH! You guessed the number in " << attempt << " attempts!" << endl;
-                return 0;
-            }
-
-            cout << "Attempts left: " << (attemptnumber - attempt) << endl;
+            cout << "BOOYAH! You guessed the number in " << attempt << " attempts!" << endl;
+            return 0;
         }
+
+        cout << "Attempts left: " << (attemptnumber - attempt) << endl;
+        cout<<endl;
     }
 
     cout << "GAME OVER! You used all 10 attempts. The number was " << randomnumber << endl;
