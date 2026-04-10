278513a786d2d417fac32111d39636e2f51afca1
stCristi <cristianstefanut60@gmail.com>
2026-04-10 12:22:33 +0200

    find the number(game) final version

--- Find the number/find_the_number.cpp
+++ Find the number/find_the_number.cpp
@@ -4,26 +4,29 @@
 using namespace std;
 
 int main() {
-    int k = 0, attempt = 0, attemptnumber = 10;
+    int number = 0, attempt = 0, attemptnumber = 10;
 
     srand(time(0));
     int randomnumber = rand() % 100 + 1;
 
     while (attempt < attemptnumber) {
-        cin >> k;
+        cin >> number;
 
-        if (k < 1 || k > 100) {
+        if (number < 1 || number > 100) {
             cout << "The number needs to be between 1 and 100" << endl;
         }
+
         else {
             attempt++;
 
-            if (k < randomnumber) {
+            if (number < randomnumber) {
                 cout << "No, the number is higher +" << endl;
             }
-            else if (k > randomnumber) {
+            
+            else if (number > randomnumber) {
                 cout << "No, the number is lower -" << endl;
             }
+            
             else {
                 cout << "BOOYAH! You guessed the number in " << attempt << " attempts!" << endl;
                 return 0;
